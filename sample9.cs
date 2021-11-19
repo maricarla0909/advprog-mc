5f76121b55482437c15a6ea984c1b6f2a54c4c37
Maricarla Reyes <75841832+maricarla0909@users.noreply.github.com>
2021-11-19 20:53:28 +0800

    Create sample9.cs

---
+++ sample9.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+		int length = 2;
+		int width = 3;
+		int area = length * width;
+		
+		Console.WriteLine("Given the following rectangle measurements: ");
+		Console.WriteLine("Length = "+ length);
+		Console.WriteLine("Width = "+ width);
+		Console.WriteLine("The area of the rectangle is: "+ area);
+		
+		
+	}
+}
