86ac1ba1d8578cdb8a3a448c78a736aab390d3af
Maricarla Reyes <75841832+maricarla0909@users.noreply.github.com>
2021-11-19 19:37:21 +0800

    Create sample3.cs

---
+++ sample3.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string fname = "Maricarla";
+    string lname = "Reyes";
+		Console.WriteLine(fname + " " + lname);
+	
+	}
+}
