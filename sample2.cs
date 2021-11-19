6fc57e0deca70bef03f7f169d1d9dc7c7955c466
Maricarla Reyes <75841832+maricarla0909@users.noreply.github.com>
2021-11-19 19:33:22 +0800

    Create sample2.cs

---
+++ sample2.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string name = "Maricarla";
+		int age = 21;
+		Console.WriteLine("Hello " + name);
+		Console.WriteLine("Your age is " + age) ;
+	}
+}
